632dc052a280ee7fe43ebcc33ff8381992bad544
zamatalo <zamatalo@gmail.com>
2025-01-02 09:55:46 +0100

    Initial commit

---
+++ backend/src/main/java/com/example/backend/BackeedApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackeedApplication.class, args);
+    }
+
+}
